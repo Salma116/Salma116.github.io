25d18fbaa9018a2870cb79d35193ee12e8a0d786
Salma <71652237+Salma116@users.noreply.github.com>
2021-04-02 15:59:11 +0200

    scroll appear added

--- js/script.js
+++ js/script.js
@@ -2,3 +2,24 @@ var hiddenmenu = $( ".listnav" );
 $( ".fa.fa-bars.fa-2x" ).on( "click", function( event ) {
   hiddenmenu.show();
 });
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 300px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
